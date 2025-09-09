222d8905e782ea065f9e6aca9b3d418b622630b7
Mert Efe Cerit <mertefecerit@gmail.com>
2025-09-09 10:31:19 +0300

    DEVELOP
    update : landing page update add sign in link for testing other users

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import XCard from "@/libs/app/components/XCard/XCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={"h-dvh bg-gray-100 flex items-center justify-center"}>
         <XCard>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, illo?</p>
+            <div className={"space-y-2"}>
+                <p>Welcome to business management app click to sign in link landing page is not ready</p>
+                <Link href={"auth/sign-in"} className={"text-sky-500 hover:text-sky-600"}>Sign in</Link>
+            </div>
         </XCard>
     </div>
   );
