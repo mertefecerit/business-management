2a64f37223b663812b07be38259c5f1c174e7c54
Mert Efe Cerit <mertefecerit@gmail.com>
2025-09-09 14:24:15 +0300

    DEVELOP
    create : fix isActive logic

--- libs/app/components/layouts/protected/ProtectedLayoutSidebar/ProtectedLayoutSidebarMenuItem.tsx
+++ libs/app/components/layouts/protected/ProtectedLayoutSidebar/ProtectedLayoutSidebarMenuItem.tsx
@@ -12,7 +12,7 @@ const ProtectedLayoutSidebarMenuItem = ({item}: { item: IMenuItem }) => {
     const pathname = usePathname()
     const [subMenuStatus, setSubMenuStatus] = useState<boolean>(false)
 
-    const isActive = subMenuStatus || pathname === item.href ? 'bg-zinc-100':'hover:bg-zinc-100'
+    const isActive = subMenuStatus || pathname === item.href || item.children?.some((child) => child.href === pathname) ? 'bg-zinc-100':'hover:bg-zinc-100'
 
     const isActiveChildRoute = (childHref:string) => {
         return pathname === childHref ? 'bg-zinc-100':'hover:bg-zinc-100'
